938d9f87d00d8045b45798d5dddafe857efb0d76
Abhinav Gorantla <abhinav20016@gmail.com>
2024-04-20 15:13:40 -0700

    Removed duplicates from  sample_3
    - Cleaned up ColumnarNestedLoopJoin constructor.

--- src/iterator/ColumnarNestedLoopJoins.java
+++ src/iterator/ColumnarNestedLoopJoins.java
@@ -13,60 +13,53 @@ public class ColumnarNestedLoopJoins extends Iterator {
     private Iterator _am2;
     private Tuple innerTuple;
     private final Tuple Jtuple;
-    private Tuple outerTuple;
-    private final CondExpr[] RightFilter;
-    private final CondExpr[] OutputFilter;
-    private final short[] InnertTargets;
+    private final CondExpr[] _rightFilter;
+    private final CondExpr[] _outFilter;
+    private final short[] _innerTargets;
     private final FldSpec[] perm_mat;
     private final FldSpec[] i_proj;
     private final String cfName;
-    private boolean done, getfromouter;
+    private boolean done;
 
-    public ColumnarNestedLoopJoins(AttrType[] in1, AttrType[] in2, Iterator am1, String relName, CondExpr[] outputFilter, CondExpr[] innerFilter, short[] innerTargets, FldSpec[] inner_Proj, FldSpec[] proj_list, Tuple proj_t) {
+    public ColumnarNestedLoopJoins(AttrType[] in1, AttrType[] in2, Iterator am1, String relName, CondExpr[] outFilter, CondExpr[] rightFilter, short[] innerTargets, FldSpec[] inner_Proj, FldSpec[] proj_list, Tuple proj_t) {
         _in1 = in1;
         _in2 = in2;
         _am1 = am1;
         _am2 = null;
         innerTuple = new Tuple();
         Jtuple = proj_t;
-        RightFilter = innerFilter;
-        OutputFilter = outputFilter;
-        InnertTargets = innerTargets;
+        _rightFilter = rightFilter;
+        _outFilter = outFilter;
+        _innerTargets = innerTargets;
         done = false;
-        getfromouter = true;
         perm_mat = proj_list;
         cfName = relName;
         i_proj = inner_Proj;
     }
 
     public Tuple get_next() throws Exception {
-        if (done)
+        if (done) {
             return null;
-        do {
-            if (getfromouter) {
-                getfromouter = false;
-                if (_am2 != null) {
-                    _am2 = null;
-                }
-                try {
-                    _am2 = new ColumnarFileScan(cfName, i_proj, InnertTargets, RightFilter);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if ((outerTuple = _am1.get_next()) == null) {
-                    done = true;
-                    return null;
-                }
-            }
+        }
 
-            while ((innerTuple = _am2.get_next()) != null) {
-                if (PredEval.Eval(OutputFilter, outerTuple, innerTuple, _in1, _in2)) {
+        while (true) {
+            // Read one outer tuple
+            Tuple outerTuple = _am1.get_next();
+            _am2 = null;
+            _am2 = new ColumnarFileScan(cfName, i_proj, _innerTargets, _rightFilter);
+
+            if (outerTuple == null) {
+                done = true;
+                return null;
+            }
+            // Check if any of the tuples in the inner relation match with the current outer tuple
+            while((innerTuple = _am2.get_next()) != null) {
+                if(PredEval.Eval(_outFilter, outerTuple, innerTuple, _in1, _in2)) {
                     Projection.Join(outerTuple, _in1, innerTuple, _in2, Jtuple, perm_mat, perm_mat.length);
                     return Jtuple;
                 }
             }
-            getfromouter = true;
-        } while (true);
+        }
     }
 
     public void close() throws JoinsException, IOException, IndexException {
